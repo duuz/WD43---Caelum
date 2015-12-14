00d13bc6c0417b1524230d2813def506c8c0d851
Eduardo Augusto <duuz@users.noreply.github.com>
2015-12-14 18:06:00 -0200

    aula 05

---
+++ rodape.php
@@ -0,0 +1,14 @@
+<footer>
+	<!-- Conteúdo do rodapé-->
+	<div class="container">
+		<img src="img/logo-rodape.png" alt="Logo Mirror Fashion">
+		<ul class="social">
+			<li>
+				<a href="http://facebook.com/mirrorfashion">FACEBOOK</a></li>
+			<li>
+				<a href="http://twitter.com/mirrorfashion">TWITER</a></li>
+			<li>
+				<a href="http://plus.google.com/mirrorfashion">GOOGLE</a></li>
+		</ul>
+	</div>
+</footer>
