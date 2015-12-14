9742dc3fcadbec5b5da18ca8d0887e31919893f8
Eduardo Augusto <duuz@users.noreply.github.com>
2015-12-14 18:07:03 -0200

    aula 05

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<!--[if lt IE 9]><script src="../js/html5shiv.min.js"></script><[endif]!-->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Mirror Fashion</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+</head>
+
+<body>
+	<?php include ("cabecalho.php");?>
+		<div class="destaque container">
+			<section class="busca">
+				<h2>BUSCA</h2>
+				<form>
+					<input type="search">
+					<input type="image" src="img/busca.png">
+				</form>
+			</section>
+			<!--fim .busca-->
+			<section class="menu-departamentos">
+				<h2>DEPARTAMENTOS</h2>
+				<nav>
+					<ul>
+						<li><a href="#">BLUSAS E CAMISETAS</a>
+							<ul>
+								<li><a href="#">Manga Curta</a></li>
+								<li><a href="#">Manga Comprida</a></li>
+								<li><a href="#">Camisa Social</a></li>
+								<li><a href="#">Camisa casual</a></li>
+
+							</ul>
+						</li>
+						<li><a href="#">CALÇAS</a></li>
+						<li><a href="#">SAIAS</a></li>
+						<li><a href="#">VESTIDOS</a></li>
+						<li><a href="#">SAPATOS</a></li>
+						<li><a href="#">BOLSAS E CARTEIRAS</a></li>
+						<li><a href="#">ACESSÓRIOS</a></li>
+
+					</ul>
+				</nav>
+			</section>
+			<!--fim .menu-departamentos-->
+
+			<!--fim .container .destaque-->
+			<img src="img/destaque-home.png" alt="Promoção: Big City Night">
+		</div>
+		<div class="container paineis">
+			<section class="painel novidades">
+				<h2>Novidades</h2>
+				<ol>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura1.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura2.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura3.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura4.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura5.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura6.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+				</ol>
+
+			</section>
+			<section class="painel mais-vendidos">
+				<h2>Mais vendidos</h2>
+				<ol>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura7.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura8.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura9.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura10.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura11.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+					<li>
+						<a href="produto.php">
+							<figure>
+								<img src="img/produtos/miniatura12.png">
+								<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+							</figure>
+						</a>
+					</li>
+				</ol>
+
+			</section>
+		</div>
+		<?php include ("rodape.php");?>
+
+</body>
+
+</html>
